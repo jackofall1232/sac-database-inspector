cbc0cc6e8c151276144f0f2767a0d6e7d457d5c2
jackofall1232 <jackofall1232@gmail.com>
2026-05-29 08:14:36 -0400

    Bump version to 1.0.0 and update requirements

--- sac-database-inspector.php
+++ sac-database-inspector.php
@@ -3,8 +3,8 @@
  * Plugin Name:       SAC Database Inspector
  * Plugin URI:        https://github.com/jackofall1232/sac-database-inspector
  * Description:       Read-only administrative utility for inspecting database tables and cache usage to help identify potential bloat.
- * Version:           0.2.0
- * Requires at least: 6.9
+ * Version:           1.0.0
+ * Requires at least: 7.0
  * Requires PHP:      7.4
  * Author:            jackofall1232
  * Author URI:        https://wordpress.org
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'WPDI_VERSION', '0.2.0' );
+define( 'WPDI_VERSION', '1.0.0' );
 define( 'WPDI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WPDI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
